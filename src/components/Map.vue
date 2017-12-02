1965a56d93ca81bcdc0c2281c9721414f75bf923
hanchengz <971340448@qq.com>
2017-12-03 00:00:00 +0800

    波谱图

---
+++ src/components/Map.vue
@@ -0,0 +1,39 @@
+<!-- Map.vue -->
+<template>
+	<div>
+		<audio src="static/music.mp3" autoplay loop controls="controls" ref="audio"></audio>
+		<canvas ref="canvas">
+			fuck you!
+		</canvas>
+	</div>
+</template>
+
+
+<script>
+
+	import music from '../utils/music'
+
+	export default {
+		data: function(){
+			return {
+
+			}
+		},
+		mounted: function(){
+
+			var canvas = this.$refs.canvas;
+			var audio = this.$refs.audio;
+			music.init(audio,canvas);
+
+
+
+
+		}
+	}
+</script>
+
+
+
+<style scoped>
+	
+</style>
